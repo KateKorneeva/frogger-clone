06ad81f5feb9796b1693299b2b49fc95987927b0
korneevaed <korneevaed@gmail.com>
2014-11-23 00:17:03 +0100

    First version of bug speed randomization

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
 // Enemies our player must avoid
-var Enemy = function(locx, locy, speed) {
+var Enemy = function(locx, locy) {
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
 
@@ -8,7 +8,8 @@ var Enemy = function(locx, locy, speed) {
     this.sprite = 'images/enemy-bug.png';
     this.x = locx;
     this.y = locy;
-    this.dx = speed;
+    this.dx = this.randomize(1,5);    
+    console.log(this.dx);
 }
 
 // Update the enemy's position, required method for game
@@ -20,12 +21,18 @@ Enemy.prototype.update = function(dt) {
     this.x = this.x + this.dx;
     if (this.x > canvas.width) {
         this.x = -10;
+        this.dx = this.randomize(1,5);
+        console.log(this.dx);
     }
 }
 
 // Draw the enemy on the screen, required method for game
 Enemy.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+}
+// Bug speed rendomizer
+Enemy.prototype.randomize = function(min, max) {
+    return Math.random() * (max - min) + min;
 }
 
 // Now write your own player class
