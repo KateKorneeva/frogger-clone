4efebbd5ae5bb05e3876add64cf29ddacb889052
korneevaed <korneevaed@gmail.com>
2014-11-22 23:54:12 +0100

    Initial commit. Bugs are moving, but the speed is constant. Character is initialized, but has no position

---
+++ js/app.js
@@ -0,0 +1,66 @@
+// Enemies our player must avoid
+var Enemy = function(locx, locy, speed) {
+    // Variables applied to each of our instances go here,
+    // we've provided one for you to get started
+
+    // The image/sprite for our enemies, this uses
+    // a helper we've provided to easily load images
+    this.sprite = 'images/enemy-bug.png';
+    this.x = locx;
+    this.y = locy;
+    this.dx = speed;
+}
+
+// Update the enemy's position, required method for game
+// Parameter: dt, a time delta between ticks
+Enemy.prototype.update = function(dt) {
+    // You should multiply any movement by the dt parameter
+    // which will ensure the game runs at the same speed for
+    // all computers.
+    this.x = this.x + this.dx;
+    if (this.x > canvas.width) {
+        this.x = -10;
+    }
+}
+
+// Draw the enemy on the screen, required method for game
+Enemy.prototype.render = function() {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+}
+
+// Now write your own player class
+// This class requires an update(), render() and
+// a handleInput() method.
+var Player = function() {
+    this.sprite = 'images/char-boy.png';
+}
+Player.prototype.update = function(dt) {
+
+}
+Player.prototype.render = function() {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+}
+Player.prototype.handleInput = function() {
+
+}
+
+// Now instantiate your objects.
+// Place all enemy objects in an array called allEnemies
+// Place the player object in a variable called player
+var allEnemies = [new Enemy(0,60,4), new Enemy(0,145,3), new Enemy(0,230,2)];
+
+var player = new Player();
+
+
+// This listens for key presses and sends the keys to your
+// Player.handleInput() method. You don't need to modify this.
+document.addEventListener('keyup', function(e) {
+    var allowedKeys = {
+        37: 'left',
+        38: 'up',
+        39: 'right',
+        40: 'down'
+    };
+
+    player.handleInput(allowedKeys[e.keyCode]);
+});
