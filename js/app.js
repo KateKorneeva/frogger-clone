4d9bc4e5720e12fec5d2e66603319e98a178e051
korneevaed <korneevaed@gmail.com>
2014-12-14 20:09:22 +0100

    Done comments for app.js

--- js/app.js
+++ js/app.js
@@ -1,27 +1,29 @@
-// Enemies our player must avoid
+// Enemies which player must avoid
 var Enemy = function() {
-    // The image/sprite for our enemies, this uses
-    // a helper we've provided to easily load images
+    // The image/sprite for enemies, this uses
+    // a helper to easily load images
     this.sprite = 'images/enemy-bug.png';
     // dTrack variable allows bugs stay at the middle of blocks
     this.dTrack = trackHeight;
-    // Position bug and give it speed value when bug is initialised
+    // Position enemy and give it speed value when emeny is initialised
     this.startBug();
 }
 
-// Update the enemy's position, required method for game
+// Update the enemy's position
 // Parameter: dt, a time delta between ticks
 Enemy.prototype.update = function(dt) {
-    // Any movement is multiplied by the dt parameter.
-    // This ensures that the game runs at the same speed
-    // for all computers.
+    // Enemy position is updated with dX until enemy
+    // reaches right border of the screen.
+    // After that bug appears at left and starts again.
+    // Due to dt multiplier the game runs at the same 
+    // speed for all computers.
     this.x = this.x + (this.dx * dt);
     if (this.x > canvas.width) {
         this.startBug();
     }
 }
 
-// Draw the enemy on the screen, required method for game
+// Draw the enemy on the screen
 Enemy.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 }
@@ -51,24 +53,32 @@ Enemy.prototype.startBug = function() {
     this.dx = this.randomizeSpeed(50,200);
 }
 
-// Now write your own player class
-// This class requires an update(), render() and
-// a handleInput() method.
+// Character which player can command
 var Player = function() {
+    // The image/sprite for character, this uses
+    // a helper to easily load images    
     this.sprite = 'images/char-boy.png';
+    // Set initial score value to 0
     this.score = 0;
+    // Position character at the bottom of the game field
     this.startPlayer();
+    // Transform keypress into movements of the character
     this.handleInput();
 }
 
-Player.prototype.update = function(dt) {
+// Displays score on each player update
+Player.prototype.update = function() {
     this.displayScore(this.score);
 }
 
+// Draw the character on the screen
 Player.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 }
 
+// Transform keypress into character position shift
+// If statements check that the character will stay 
+// inside the game field after it moves.
 Player.prototype.handleInput = function(key) {
     this.dx = trackWidth;
     this.dy = trackHeight;
@@ -111,86 +121,105 @@ Player.prototype.handleInput = function(key) {
     }
 }
 
+// Position character at the bottom of the game field
 Player.prototype.startPlayer = function() {
-    this.x = 200;
-    this.y = 415;
-    // alert("Your score is " + this.score);
+    this.x = playerStartX;
+    this.y = playerStartY;
 }
 
+// Summarise score which player had before (score)
+// with score gained by current move (dScore)
 Player.prototype.countScore = function(dScore) {
     this.dScore = dScore;
     this.score = this.score + this.dScore;
 }
+// Send score value to the div above game field
 Player.prototype.displayScore = function(score) {
     scoreDispl.innerHTML = 'Your score is: ' + score;
 }
 
+// Gems which player should collect to
+// gain score
 var Gem = function() {
-    this.x = 0;
-    this.y = 0;
-    this.gemFitX = 5;
-    this.gemFitY = 25;
     this.randomizeAppear();
 }
 
+// Randomisation of gem location
 Gem.prototype.randomizeAppear = function() {
     this.xCol = randomize(0,6) - 1;
     this.x = this.xCol * trackWidth;
     this.yCol = randomize(0,4);
-    this.y = this.yCol * trackHeight - this.gemFitY;
+    this.y = this.yCol * trackHeight - gemFitY;
 }
 
+// Draw the gem on the screen
 Gem.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 }
 
 var GemOrange = function() {
+    // This inherits properties from Gem superclass
     Gem.call(this);
     this.sprite = 'images/gem-orange.png';
-    this.dScoreGem = 10;
+    // Set score quantity gained for orange
+    // gem collection to lowScore variable value
+    this.dScoreGem = lowScore;
 }
 
+// This inherits methods from Gem superclass
 GemOrange.prototype = Object.create(Gem.prototype);
 GemOrange.prototype.constructor = Gem;
 
 var GemGreen = function() {
+    // This inherits properties from Gem superclass
     Gem.call(this);
     this.sprite = 'images/gem-green.png';
-    this.dScoreGem = 20;
+    // Set score quantity gained for green
+    // gem collection to middle variable value    
+    this.dScoreGem = middleScore;
 }
 
+// This inherits methods from Gem superclass
 GemGreen.prototype = Object.create(Gem.prototype);
 GemGreen.prototype.constructor  = Gem;
 
 var GemBlue = function() {
+    // This inherits properties from Gem superclass
     Gem.call(this);
     this.sprite = 'images/gem-blue.png';
-    this.dScoreGem = 30;
+    // Set score quantity gained for blue
+    // gem collection to highScore variable value    
+    this.dScoreGem = highScore;
 }
 
+// This inherits methods from Gem superclass
 GemBlue.prototype = Object.create(Gem.prototype);
 GemBlue.prototype.constructor  = Gem;
 
-// Default global random function
+// Global random function. Used for different prototypes
 var randomize = function(max, min) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
+// Set of global variables with digital values.
+var trackWidth = 101,
+    trackHeight = 83,
+    firstTrackY = 76,
+    playerStartX = 200,
+    playerStartY = 415,
+    gemFitX = 5,
+    gemFitY = 25,
+    lowScore = 10,
+    middleScore = 20,
+    highScore = 30;
 
-var trackWidth = 101;
-var trackHeight = 83;
-var firstTrackY = 76;
-var la = true;
-
-// Now instantiate your objects.
-// Place all enemy objects in an array called allEnemies
-// Place the player object in a variable called player
+// Here objects are instantiated.
 var allEnemies = [new Enemy(), new Enemy(), new Enemy()];
 var player = new Player();
 var gems = [new GemOrange(), new GemGreen(), new GemBlue()];
 
-// This listens for key presses and sends the keys to your
-// Player.handleInput() method. You don't need to modify this.
+// This listens for key presses and sends the keys to
+// Player.handleInput() method.
 document.addEventListener('keyup', function(e) {
     var allowedKeys = {
         37: 'left',
@@ -198,7 +227,5 @@ document.addEventListener('keyup', function(e) {
         39: 'right',
         40: 'down'
     };
-
     player.handleInput(allowedKeys[e.keyCode]);
 });
-
